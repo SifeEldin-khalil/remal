accf2bdbf5679dbd81511d428b07f1440fc4d361
Menna <mmorsi@ejada.com>
2021-04-12 19:25:32 +0200

    add company and contact us apis

---
+++ remal-back/src/routes/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const fileController = require("../controller/file.controller");
+const companyController = require("../controller/company.controller");
+// const { route } = require("../../example");
+// const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+
+// var bodyParser = require('body-parser');
+
+let routes = (app) => {
+    // router.post("/Api/Upload", fileController.upload);
+    // router.get("/files", controller.getListFiles);
+    router.get("/Api/Files", fileController.download);
+    router.get("/Api", companyController.testApi);
+    router.get("/Api/Company", companyController.getCompany);
+    router.get("/Api/Careers", companyController.getAllCareers);
+    router.post("/Api/AddContact", companyController.addContact);
+    // app.use(bodyParser.json());
+    // app.use(bodyParser.json({ limit: '50mb', extended: true }));
+    // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    // app.use(cookieParser());
+    // app.use(cors());
+    app.use(router);
+
+
+
+};
+
+module.exports = routes;
